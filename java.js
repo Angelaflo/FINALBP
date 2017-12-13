7ebe05d2babfed0782f1955d1aba60be15922cb1
Angelaflo <Angela_flores@davincischools.org>
2017-12-13 14:14:12 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+function showJennie(elem) {
+    elem.classList.toggle("tubby"); 
+} 
+
+function showWhistle(elem) {
+    elem.classList.toggle("tubby2"); 
+} 
+
+function showRose(elem) {
+    elem.classList.toggle("tubby3"); 
+} 
+
+function showPWF(elem) {
+    elem.classList.toggle("tubby4"); 
+} 
+
+function showJisoo(elem) {
+    elem.classList.toggle("tubby5"); 
+} 
+
+function showBoom(elem) {
+    elem.classList.toggle("tubby6"); 
+} 
+
+function showLisa(elem) {
+    elem.classList.toggle("tubby7"); 
+} 
+
+function showAIYL(elem) {
+    elem.classList.toggle("tubby8"); 
+} 
